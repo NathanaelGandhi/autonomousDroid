f04d9993b5c3bfdc80f600cb12d05d29bb73d2a8
Nathanael Gandhi <nathanael.gandhi@uts.edu.au>
2020-01-16 15:47:24 +1100

    data_collection started, incomplete. Commiting to move to gitflow workflow.

--- autonomous_droid_main/src/data_collection.cpp
+++ autonomous_droid_main/src/data_collection.cpp
@@ -1,7 +1,34 @@
-/*#include <ros/ros.h>
+#include <ros/ros.h>
+#include "autonomous_droid_main/FusedData.h"
 
-int main(){
+void imuCallback(const std_msgs::String::ConstPtr& msg){
+        ROS_INFO("[DC] IMU Callback");
+}
+
+void cameraCallback(const std_msgs::String::ConstPtr& msg){
+        ROS_INFO("[DC] Camera Callback");
+}
+
+int main(int argc char **argv){
 	ROS_INFO_STREAM("[DC] Starting data collection node.");
-	ros::spin();
+	ros::init(argc, argv, "data_collection");
+        ros::Nodehandle n;
+
+        //Subscribers
+        ros::Subscriber imu_sub = n.subscribe("imu", 1, imuCallback);
+        ros::Subscriber camera_sub = n.subscribe("camera", 1, imuCallback);
+
+        //Publishers
+        ros::Publisher fused_data_pub = n.advertise<autonomous_droid_main::FusedData>("fused_data",1000);
+
+        ros::Rate loop_rate(30);
+        int count = 0;
+        while(ros::ok()){
+                autonomous_droid_main::FusedData msg;
+                fused_data_pub.publish(msg)
+                ros::spinOnce();
+                loop_rate.sleep();
+                count++;
+        }
 	return 0;
-}*/
+}
